e00396a4eb134be0c5fa098868e898ac4947ec9c
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-01-21 16:41:57 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,9 @@ function factors(num) {
     }
     return result;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
